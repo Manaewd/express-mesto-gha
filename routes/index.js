b329ab2650d0c5c6f4f23b10eddfa0263e5f26d7
Denis Manaev <manaewd@yandex.ru>
2023-06-28 23:28:26 +0700

    забыл исправить общую ошибку

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const auth = require('../middlewares/auth');
+const NotFoundError = require('../errors/not-found-error');
 const { createUser, login } = require('../controllers/users');
-
-const { ERROR_NOT_FOUND } = require('../utils/utils');
 
 router.post('/signin', login);
 router.post('/signup', createUser);
@@ -14,8 +13,8 @@ router.use(auth);
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 
-router.use((req, res) => {
-  res.status(ERROR_NOT_FOUND).send({ message: 'Что-то пошло не так' });
+router.use((req, res, next) => {
+  next(new NotFoundError('Что-то пошло не так'));
 });
 
 module.exports = router;
